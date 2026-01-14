6064ae207eceb25e018abb01d8515ea8ec269727
Alina Raltsevich <alraltse@c3r8s1.42warsaw.pl>
2026-01-14 14:59:19 +0100

    Remove class

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -1,48 +1,21 @@
 #include <iostream>
-#include <string>
-#include <ctype.h>
+#include <cctype>
 
-class ToUpper {
-    private:
-        std::string default_message;
-    
-    public:
-        ToUpper() : default_message("* LOUD AND UNBEARABLE FEEDBACK NOISE *") {};
+int main(int argc, char *argv[]) {
+    std::string str;
+    std::string temp_str;
 
-    private:
-        void convert_to_uppercase(int size, char** message) {
-            int str_idx;
-            int char_idx;
-            std::string str;
-            std::string temp_str;
-
-            str_idx = 1;
-            while (message[str_idx]) {
-                str = message[str_idx];
-                char_idx = 0;
-                temp_str = "";
-                while (str[char_idx]) {
-                    temp_str += (char)toupper(str[char_idx]);
-                    char_idx++;
-                }
-                std::cout << temp_str;
-                str_idx++;
-            }
-            std::cout << std::endl;
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else {
+        for (int i = 1; i < argc; i++) {
+            str = argv[i];
+            temp_str = "";
+            for (int char_idx = 0; char_idx < str.length(); char_idx++)
+                temp_str += (char)toupper(str[char_idx]);
+            std::cout << temp_str;
         }
-
-    public:
-        void str_to_uppercase(int size, char** message) {
-            if (size == 1)
-                std::cout << default_message << std::endl;
-            else
-                convert_to_uppercase(size, message);
+        std::cout << std::endl;
     }
-};
-
-int main(int argc, char **argv) {
-    ToUpper test;
-    test.str_to_uppercase(argc, argv);
-
     return 0;
 }
