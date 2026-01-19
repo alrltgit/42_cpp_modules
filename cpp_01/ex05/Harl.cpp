4a84602453a74355ec672e1ee7fc9c2a25e0bba3
Alina Ralcewicz <ralcevichalina70@gmail.com>
2026-01-19 10:34:12 +0100

    Complete ex05

---
+++ cpp_01/ex05/Harl.cpp
@@ -0,0 +1,47 @@
+#include "Harl.hpp"
+
+Harl::Harl() {
+    ptrToFuncs[0] = &Harl::debug;
+    ptrToFuncs[1] = &Harl::info;
+    ptrToFuncs[2] = &Harl::warning;
+    ptrToFuncs[3] = &Harl::error;
+};
+
+Harl::~Harl() {};
+
+void Harl::debug() {
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
+        << std::endl;
+};
+
+void Harl::info() {
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+        << std::endl;
+}
+
+void Harl::warning() {
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month."
+        << std::endl;
+}
+
+void Harl::error() {
+    std::cout << "This is unacceptable! I want to speak to the manager now."
+        << std::endl;
+}
+
+void Harl::complain(std::string level) {
+    // In C++, functions can be treated as objects with 
+    // the help of function pointers. A function pointer 
+    // to a member function is a pointer that can refer to 
+    // a member function of a specific class.
+    int i = 4;
+
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    while (i--) {
+        if (levels[i] == level) {
+            (this->*ptrToFuncs[i])(); // calling a member function through a pointer.
+            return ;
+        }
+    }
+    std::cout << "Wrong level!" << std::endl;
+}
