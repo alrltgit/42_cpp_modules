100a0a4c4e519045c5888456c5b30915030d722c
Alina Ralcewicz <ralcevichalina70@gmail.com>
2026-01-28 18:16:02 +0100

    Complete ex02

---
+++ cpp_04/ex02/main.cpp
@@ -0,0 +1,52 @@
+#include "inc/Dog.hpp"
+#include "inc/Animal.hpp"
+#include "inc/Cat.hpp"
+#include "inc/Brain.hpp"
+
+int main()
+{
+    // Abstract classes cannot be instantiated, 
+    // but they can be used as pointer or reference types.
+
+    // Animal* arr = new Animal();
+    // Animal arr;
+
+    Animal* arr[4];
+    arr[0] = new Cat();
+    arr[1] = new Cat();
+    arr[2] = new Dog();
+    arr[3] = new Dog();
+
+    for (int i = 0; i < 4; i++) {
+        arr[i]->makeSound();
+    }
+
+    for (int i = 0; i < 4; i++) {
+        delete arr[i];
+    }
+
+    Cat* cat = new Cat();
+    Dog* dog = new Dog();
+
+    std::cout << cat->getType() << std::endl;
+    std::cout << dog->getType() << std::endl;
+
+    cat->makeSound();
+    dog->makeSound();
+
+    cat->getBrain()->setIdea(0, "Cat: Idea #1");
+    cat->getBrain()->setIdea(1, "Cat: Idea #2");
+    dog->getBrain()->setIdea(0, "Dog: Idea #1");
+    dog->getBrain()->setIdea(1, "Dog: Idea #2");
+
+    std::cout << "----- Cat ideas -----" << std::endl;
+    cat->getBrain()->displayIdea();
+
+    std::cout << "----- Dog ideas -----" << std::endl;
+    dog->getBrain()->displayIdea();
+
+    delete cat;
+    delete dog;
+
+    return 0;
+}
---
+++ cpp_04/ex02/src/Dog.cpp
@@ -0,0 +1,38 @@
+#include "../inc/Dog.hpp"
+#include "../inc/Animal.hpp"
+#include "../inc/Brain.hpp"
+
+Dog::Dog() : Animal() {
+    type = "Dog";
+    br = new Brain;
+    std::cout << "Dog: default constructor called" << std::endl;
+};
+
+Dog::Dog(const Dog& other) : Animal(other) {
+    std::cout << "Dog: copy constructor called" << std::endl;
+    *this = other;
+}
+
+Dog& Dog::operator = (const Dog& other) {
+    std::cout << "Dog: copy assignment operator called" << std::endl;
+    if (this != &other) {
+        type = other.type;
+        if (br)
+            delete br;
+        br = new Brain(*other.br);
+    }
+    return *this;
+}
+
+Dog::~Dog() {
+    std::cout << "Dog: destructor called" << std::endl;
+    delete br;
+}
+
+Brain* Dog::getBrain() const {
+    return br;
+}
+
+void Dog::makeSound() const {
+    std::cout << "Dog makes a sound: Woof Woof!" << std::endl;
+}
