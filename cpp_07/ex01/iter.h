6235765aee67ac58a5b282d064dc789f3c999232
Alina Ralcewicz <ralcevichalina70@gmail.com>
2026-04-03 15:50:48 +0200

    Budfix: use function instead of function pointer as parameter. Add template function that supports const T *list

--- cpp_07/ex01/iter.h
+++ cpp_07/ex01/iter.h
@@ -4,7 +4,14 @@
 #include <iostream>
 
 template <typename T, typename F>
-void iter(T *list, const size_t listLength, F(*f)) {
+void iter(T *list, const size_t listLength, F f) {
+    for (size_t i = 0; i < listLength; ++i) {
+        f(list[i]);
+    }
+}
+
+template <typename T, typename F>
+void iter(const T *list, const size_t listLength, F f) {
     for (size_t i = 0; i < listLength; ++i) {
         f(list[i]);
     }
